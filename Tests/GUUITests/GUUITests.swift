bae060ad7a2bc7aa8132cde7bcc212f97388b77c
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-08-18 17:26:46 +1000

    Moved global functions into extensions and made tests.

--- Tests/GUUITests/GUUITests.swift
+++ Tests/GUUITests/GUUITests.swift
@@ -1,11 +1,16 @@
-    import XCTest
-    @testable import GUUI
 
-    final class GUUITests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            //XCTAssertEqual(GUUI().text, "Hello, World!")
-        }
-    }
+@testable import GUUI
+import XCTest
+
+/// Test class for GUUI.
+final class GUUITests: XCTestCase {
+
+    /// Example test.
+    // func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        // XCTAssertEqual(GUUI().text, "Hello, World!")
+    // }
+
+}
