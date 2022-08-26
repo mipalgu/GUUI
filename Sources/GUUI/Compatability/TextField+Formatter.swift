ca9263d272840c377b264593a20f3107563a1fed
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-08-27 09:01:17 +1000

    Created onEditingChanged and onCommit stubs for working with text fields with formatters

--- Sources/GUUI/Compatability/TextField+Formatter.swift
+++ Sources/GUUI/Compatability/TextField+Formatter.swift
@@ -8,6 +8,73 @@ public extension TextField {
     init<S, V>(
         _ title: S,
         value: Binding<V>,
+        formatter: NumberFormatter,
+        onCommit: @escaping () -> Void
+    ) where S: StringProtocol, V: _ObjectiveCBridgeable, V._ObjectiveCType == NSNumber, Label == Text {
+        let binding = Binding<String> {
+            formatter.string(from: value.wrappedValue._bridgeToObjectiveC()) ?? ""
+        } set: {
+            guard let num = formatter.number(from: $0) else {
+                return
+            }
+            var val: V? = value.wrappedValue
+            V._forceBridgeFromObjectiveC(num, result: &val)
+            guard let val = val else {
+                return
+            }
+            value.wrappedValue = val
+        }
+        self.init(title, text: binding, onCommit: onCommit)
+    }
+
+    init<S, V>(
+        _ title: S,
+        value: Binding<V>,
+        formatter: NumberFormatter,
+        onEditingChanged: @escaping (Bool) -> Void,
+        onCommit: @escaping () -> Void
+    ) where S: StringProtocol, V: _ObjectiveCBridgeable, V._ObjectiveCType == NSNumber, Label == Text {
+        let binding = Binding<String> {
+            formatter.string(from: value.wrappedValue._bridgeToObjectiveC()) ?? ""
+        } set: {
+            guard let num = formatter.number(from: $0) else {
+                return
+            }
+            var val: V? = value.wrappedValue
+            V._forceBridgeFromObjectiveC(num, result: &val)
+            guard let val = val else {
+                return
+            }
+            value.wrappedValue = val
+        }
+        self.init(title, text: binding, onEditingChanged: onEditingChanged, onCommit: onCommit)
+    }
+
+    init<S, V>(
+        _ title: S,
+        value: Binding<V>,
+        formatter: NumberFormatter,
+        onEditingChanged: @escaping (Bool) -> Void
+    ) where S: StringProtocol, V: _ObjectiveCBridgeable, V._ObjectiveCType == NSNumber, Label == Text {
+        let binding = Binding<String> {
+            formatter.string(from: value.wrappedValue._bridgeToObjectiveC()) ?? ""
+        } set: {
+            guard let num = formatter.number(from: $0) else {
+                return
+            }
+            var val: V? = value.wrappedValue
+            V._forceBridgeFromObjectiveC(num, result: &val)
+            guard let val = val else {
+                return
+            }
+            value.wrappedValue = val
+        }
+        self.init(title, text: binding, onEditingChanged: onEditingChanged)
+    }
+
+    init<S, V>(
+        _ title: S,
+        value: Binding<V>,
         formatter: NumberFormatter
     ) where S: StringProtocol, V: _ObjectiveCBridgeable, V._ObjectiveCType == NSNumber, Label == Text {
         let binding = Binding<String> {
